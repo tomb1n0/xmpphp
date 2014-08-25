bcf8936c3fed93ef1a72401e823abdfcd6bf8f9d
Charles-Edouard Coste <cc@synap.fr>
2014-08-25 13:33:59 +0200

    PSR-2 conventions

--- examples/sendmessage_example.php
+++ examples/sendmessage_example.php
@@ -7,7 +7,7 @@ include 'XMPPHP/XMPP.php';
 
 #Use XMPPHP_Log::LEVEL_VERBOSE to get more logging for error reports
 #If this doesn't work, are you running 64-bit PHP with < 5.2.6?
-$conn = new XMPPHP_XMPP('talk.google.com', 5222, 'username', 'password', 'xmpphp', 'gmail.com', $printlog=false, $loglevel=XMPPHP_Log::LEVEL_INFO);
+$conn = new XMPPHP_XMPP('talk.google.com', 5222, 'username', 'password', 'xmpphp', 'gmail.com', $printlog = false, $loglevel = XMPPHP_Log::LEVEL_INFO);
 
 try {
     $conn->connect();
@@ -15,6 +15,7 @@ try {
     $conn->presence();
     $conn->message('someguy@someserver.net', 'This is a test message!');
     $conn->disconnect();
-} catch(XMPPHP_Exception $e) {
+
+} catch (XMPPHP_Exception $e) {
     die($e->getMessage());
 }
